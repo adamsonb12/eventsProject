c5355f4696aacf89fcf334d78f28640b28314da4
Brett Adamson <adamsonb12@gmail.com>
2017-10-10 11:12:39 -0600

    google oauth connecting

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '411955820859-f4td9snvm54tgphclnrp17uba2950kvb.apps.googleusercontent.com',
+  googleClientSecret: 'mStMUba0ViyTDGVk13d8h0KT',
+  mongoURI: 'mongodb://eventSuperAdmin:Safe1234@ds013475.mlab.com:13475/event-project',
+  cookieKey: 'laksdf2o3inrGOasdfkjnaskdjfBYUa;skdjnfasdCougarsas89u98dirtyasdnkfyewtsaksldnfssuckakndfuw23'
+};
